f3ef71588eeb6730f0c49ea859bf8964764ffedf
Jerry Cao <yilufeng0@gmail.com>
2015-02-04 11:08:18 +0800

    test the deletefunc be ok

--- WebContent/js/forWebAction.js
+++ WebContent/js/forWebAction.js
@@ -4,12 +4,13 @@
  */
 
 
+//使用模态框显示图片
 function dispImg (id) {
     var imgTag = document.getElementById("modalImg");
     imgTag.src= "exhibition.jsp?id="+id;    //此处添加获取图片的地址并在此处将图片ID传给后台
      $("#myModal").modal("toggle");
   }
-
+//创建新帐户
 function createNewAcc(){
 	var account= document.getElementById("account").value;
 	var passwd= document.getElementById("passwd").value;
@@ -26,6 +27,15 @@ function createNewAcc(){
 	}
 }
 
+//编辑条目信息
+function editItem(id){
+	var xmlhttp=createXMLHTTP();
+	var modalTag = document.getElementById();
+	
+	
+}
+
+//删除对应条目后的序号重新编排
 function afterDelItem(){
 	//完成条目删除后的表格刷新     同时将表格中的序号根据重新生成
 	var trs=document.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
@@ -34,28 +44,39 @@ function afterDelItem(){
 		trs.getElementsByTagName("td")[0].innerHTML=i+1;
 	}
 }
+
+//删除特定的序号的条目
 function deletedItem(id){
 	var tagid=document.getElementById(id);
 	tagid.remove();
 }
+
+
+
+//使用ajax进行远程服务器条目删除请求，并对返回的结果进行处理
 function deleteItem(id){
-	var xmlhttp = createXMLHTTP();	
+	
+	var xmlhttp = createXMLHTTP();
+	var ID=String(id);
 	xmlhttp.open("POST", "deleteApp.jsp", true);
-	xmlhttp.onreadystatechange=function(){
-		if(xmlhttp.readyState==4 && xmlhttp.status==200){
-			if(xmlhttp.responseText == "1"){
-				deletedItem(id);
+	xmlhttp.onreadystatechange=function(){	
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			var result=xmlhttp.responseText.replace(/[\r\n]/g,"");
+			if(result == "1"){				
+				deletedItem(ID);
 				afterDelItem();
-			}else if(xmlhttp.responseText =="0"){ //提示删除错误
+			}else { //提示删除错误
 				
 			}
 			
-		}		
+		}
 	};
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xmlhttp.send("id="+id);
+	xmlhttp.send("id="+ID);
 }
 
+
+//生成ajax连接
 function createXMLHTTP(){
 	var xmlHttp=null;
 	if (window.XMLHttpRequest) {
